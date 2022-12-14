ee53a1fd292cec5f8b975593fad37d57736bcbbb
fikirchanie <fkrchanie@gmail.com>
2022-12-14 14:02:23 -0800

    1-alphabet

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,6 @@
-#ifndef MAIN_H
+
 #define MAIN_H
 #include <stdio.h>
-
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -16,4 +15,3 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 
-#endif
