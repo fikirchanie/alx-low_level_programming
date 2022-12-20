19d9dcaac040cf6e8b29d6420268622043692001
fikirchanie <fkrchanie@gmail.com>
2022-12-20 04:50:50 -0800

    9-strcpy.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,23 +1,22 @@
 #include "main.h"
-#include <stdio.h>
 /**
- *print_array - prints n element of an array of integers
- *Owned by Bwave
- *@a: int to check
- *@n: int to check
- *Return: 0 is success
+ *print_array - prints n elements of an array of integers
+ *@a: A pointer to an int that will be updated/ changed
+ *@n: return value n
+ *Return: void which ,means our answer is correct
  */
 void print_array(int *a, int n)
 {
-int x
-for (x = 0; x < n; x++)
-if (x != n - 1)
-printf("%d, ", a[x]);
+int inc;
+for (inc = 0; inc < n ; inc++)
+{
+if (inc != n - 1)
+printf("%d, ", a[inc]);
 else
-printf("%d", a[x]);
+printf("%d", a[inc]);
+}
 printf("\n");
 }
-
 
 
 
